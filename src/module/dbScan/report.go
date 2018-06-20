dd7a153273f70dec259c928686272c2b7f258f41
trungnexle <trungdn@nexlesoft.com>
2018-06-20 16:24:39 +0700

    commit

--- src/module/dbScan/report.go
+++ src/module/dbScan/report.go
@@ -15,6 +15,16 @@ var REDIS_BTC_WITHDRAW = "btc_withdraw"
 var REDIS_BTC_CURRENT = "btc_current"
 var REDIS_BTC_FEES = "btc_fees"
 
+var REDIS_LTC_DEPOSIT = "ltc_deposit"
+var REDIS_LTC_WITHDRAW = "ltc_withdraw"
+var REDIS_LTC_CURRENT = "ltc_current"
+var REDIS_LTC_FEES = "ltc_fees"
+
+var REDIS_BCH_DEPOSIT = "bch_deposit"
+var REDIS_BCH_WITHDRAW = "bch_withdraw"
+var REDIS_BCH_CURRENT = "bch_current"
+var REDIS_BCH_FEES = "bch_fees"
+
 var REDIS_ETH_DEPOSIT = "eth_deposit"
 var REDIS_ETH_WITHDRAW = "eth_withdraw"
 var REDIS_ETH_CURRENT = "eth_current"
@@ -25,10 +35,90 @@ var BTC_WITHDRAW float64
 var BTC_CURRENT float64
 var BTC_FEES float64
 
+var LTC_DEPOSIT float64
+var LTC_WITHDRAW float64
+var LTC_CURRENT float64
+var LTC_FEES float64
+
+var BCH_DEPOSIT float64
+var BCH_WITHDRAW float64
+var BCH_CURRENT float64
+var BCH_FEES float64
+
 var ETH_DEPOSIT float64
 var ETH_WITHDRAW float64
 var ETH_CURRENT float64
 var ETH_FEES float64
+
+/*---------------------------------------------------------------------------*/
+
+func Report_Deposit(coin string, de float64) {
+
+	switch coin {
+	case "BTC":
+		{
+			BTC_DEPOSIT += de
+			SaveReport_BTC_Deposit(BTC_DEPOSIT)
+		}
+	case "ETH":
+		{
+			ETH_DEPOSIT += de
+			SaveReport_ETH_Deposit(ETH_DEPOSIT)
+		}
+	}
+
+}
+
+func Report_Withdraw(coin string, with float64) {
+
+	switch coin {
+	case "BTC":
+		{
+			BTC_WITHDRAW += with
+			SaveReport_BTC_Withdraw(BTC_WITHDRAW)
+		}
+	case "ETH":
+		{
+			ETH_WITHDRAW += with
+			SaveReport_ETH_Withdraw(ETH_WITHDRAW)
+		}
+	}
+
+}
+
+func Report_Fees(coin string, fees float64) {
+
+	switch coin {
+	case "BTC":
+		{
+			BTC_FEES += math.Abs(fees)
+			SaveReport_BTC_Fees(BTC_FEES)
+		}
+	case "ETH":
+		{
+			ETH_FEES += fees
+			SaveReport_ETH_Fees(ETH_FEES)
+		}
+	}
+
+}
+
+func Report_Current(coin string) {
+
+	switch coin {
+	case "BTC":
+		{
+			BTC_CURRENT = BTC_DEPOSIT - BTC_WITHDRAW
+			SaveReport_BTC_Current(BTC_CURRENT)
+		}
+	case "ETH":
+		{
+			ETH_CURRENT = ETH_DEPOSIT - ETH_WITHDRAW
+			SaveReport_ETH_Current(ETH_CURRENT)
+		}
+	}
+
+}
 
 func LoadDeposit() {
 
@@ -73,6 +163,8 @@ func SaveDeposit(de Deposit) {
 
 	fmt.Println("saveDeposit : ", de.AddressDeposit)
 }
+
+/*---------------------------------------------------------------------------*/
 
 func LoadReport_BTC() {
 
@@ -162,6 +254,190 @@ func SaveReport_BTC_Fees(fees float64) {
 
 	fmt.Println("SaveReport_BTC_Fees : ", REDIS_BTC_FEES, fees)
 }
+
+/*---------------------------------------------------------------------------*/
+
+func LoadReport_LTC() {
+
+	de, err1 := redis.Session.Get(REDIS_LTC_DEPOSIT).Result()
+	if err1 != nil {
+		fmt.Println("Error Load Report LTC Deposit")
+		return
+	}
+
+	with, err2 := redis.Session.Get(REDIS_LTC_WITHDRAW).Result()
+	if err2 != nil {
+		fmt.Println("Error Load Report LTC Withdraw")
+		return
+	}
+
+	curr, err3 := redis.Session.Get(REDIS_LTC_CURRENT).Result()
+	if err3 != nil {
+		fmt.Println("Error Load Report LTC Current")
+		return
+	}
+
+	fees, err4 := redis.Session.Get(REDIS_LTC_FEES).Result()
+	if err4 != nil {
+		fmt.Println("Error Load Report LTC Fees")
+		return
+	}
+
+	df, _ := strconv.ParseFloat(de, 64)
+	LTC_DEPOSIT = df
+
+	wf, _ := strconv.ParseFloat(with, 64)
+	LTC_WITHDRAW = wf
+
+	cf, _ := strconv.ParseFloat(curr, 64)
+	LTC_CURRENT = cf
+
+	bf, _ := strconv.ParseFloat(fees, 64)
+	LTC_FEES = bf
+
+	fmt.Println("Redis LTC Deposit : ", de, "  - Withdraw : ", with, "  - Current : ", curr, "  - Fees : ", fees)
+}
+
+func SaveReport_LTC_Deposit(de float64) {
+
+	b, _ := json.Marshal(de)
+
+	err1 := redis.Session.Set(REDIS_LTC_DEPOSIT, b, 0).Err()
+	if err1 != nil {
+		fmt.Println("Error Save Report LTC Deposit")
+	}
+
+	fmt.Println("SaveReport_LTC_Deposit : ", REDIS_LTC_DEPOSIT, de)
+}
+
+func SaveReport_LTC_Withdraw(with float64) {
+
+	b, _ := json.Marshal(with)
+
+	err2 := redis.Session.Set(REDIS_LTC_WITHDRAW, b, 0).Err()
+	if err2 != nil {
+		fmt.Println("Error Save Report LTC Withdraw")
+	}
+
+	fmt.Println("SaveReport_LTC_Withdraw : ", REDIS_LTC_WITHDRAW, with)
+}
+
+func SaveReport_LTC_Current(curr float64) {
+
+	b, _ := json.Marshal(curr)
+
+	err3 := redis.Session.Set(REDIS_LTC_CURRENT, b, 0).Err()
+	if err3 != nil {
+		fmt.Println("Error Save Report LTC Current")
+	}
+
+	fmt.Println("SaveReport_LTC_Current : ", REDIS_LTC_CURRENT, curr)
+}
+
+func SaveReport_LTC_Fees(fees float64) {
+
+	b, _ := json.Marshal(fees)
+
+	err4 := redis.Session.Set(REDIS_LTC_FEES, b, 0).Err()
+	if err4 != nil {
+		fmt.Println("Error Save Report LTC Fees")
+	}
+
+	fmt.Println("SaveReport_LTC_Fees : ", REDIS_LTC_FEES, fees)
+}
+
+/*---------------------------------------------------------------------------*/
+
+func LoadReport_BCH() {
+
+	de, err1 := redis.Session.Get(REDIS_BCH_DEPOSIT).Result()
+	if err1 != nil {
+		fmt.Println("Error Load Report BCH Deposit")
+		return
+	}
+
+	with, err2 := redis.Session.Get(REDIS_BCH_WITHDRAW).Result()
+	if err2 != nil {
+		fmt.Println("Error Load Report BCH Withdraw")
+		return
+	}
+
+	curr, err3 := redis.Session.Get(REDIS_BCH_CURRENT).Result()
+	if err3 != nil {
+		fmt.Println("Error Load Report BCH Current")
+		return
+	}
+
+	fees, err4 := redis.Session.Get(REDIS_BCH_FEES).Result()
+	if err4 != nil {
+		fmt.Println("Error Load Report BCH Fees")
+		return
+	}
+
+	df, _ := strconv.ParseFloat(de, 64)
+	BCH_DEPOSIT = df
+
+	wf, _ := strconv.ParseFloat(with, 64)
+	BCH_WITHDRAW = wf
+
+	cf, _ := strconv.ParseFloat(curr, 64)
+	BCH_CURRENT = cf
+
+	bf, _ := strconv.ParseFloat(fees, 64)
+	BCH_FEES = bf
+
+	fmt.Println("Redis BCH Deposit : ", de, "  - Withdraw : ", with, "  - Current : ", curr, "  - Fees : ", fees)
+}
+
+func SaveReport_BCH_Deposit(de float64) {
+
+	b, _ := json.Marshal(de)
+
+	err1 := redis.Session.Set(REDIS_BCH_DEPOSIT, b, 0).Err()
+	if err1 != nil {
+		fmt.Println("Error Save Report BCH Deposit")
+	}
+
+	fmt.Println("SaveReport_BCH_Deposit : ", REDIS_BCH_DEPOSIT, de)
+}
+
+func SaveReport_BCH_Withdraw(with float64) {
+
+	b, _ := json.Marshal(with)
+
+	err2 := redis.Session.Set(REDIS_BCH_WITHDRAW, b, 0).Err()
+	if err2 != nil {
+		fmt.Println("Error Save Report BCH Withdraw")
+	}
+
+	fmt.Println("SaveReport_BCH_Withdraw : ", REDIS_BCH_WITHDRAW, with)
+}
+
+func SaveReport_BCH_Current(curr float64) {
+
+	b, _ := json.Marshal(curr)
+
+	err3 := redis.Session.Set(REDIS_BCH_CURRENT, b, 0).Err()
+	if err3 != nil {
+		fmt.Println("Error Save Report BCH Current")
+	}
+
+	fmt.Println("SaveReport_BCH_Current : ", REDIS_BCH_CURRENT, curr)
+}
+
+func SaveReport_BCH_Fees(fees float64) {
+
+	b, _ := json.Marshal(fees)
+
+	err4 := redis.Session.Set(REDIS_BCH_FEES, b, 0).Err()
+	if err4 != nil {
+		fmt.Println("Error Save Report BCH Fees")
+	}
+
+	fmt.Println("SaveReport_BCH_Fees : ", REDIS_BCH_FEES, fees)
+}
+
+/*---------------------------------------------------------------------------*/
 
 func LoadReport_ETH() {
 
--- src/module/ltcScan/ltcScan.go
+++ src/module/ltcScan/ltcScan.go
@@ -1,1 +1,98 @@
 package ltcScan
+
+import (
+	"bytes"
+	"encoding/json"
+	"lib/btc"
+	"time"
+
+	"module/dbScan"
+
+	"github.com/ltcsuite/ltcd/wire"
+)
+
+var MapBlock = map[string]interface{}{}
+
+func Start() {
+
+	update()
+}
+
+func update() {
+
+	go func() {
+		for {
+
+			getBlock()
+
+			time.Sleep(10000 * time.Millisecond)
+
+		}
+	}()
+}
+
+func getBlock() {
+
+	blockHeight := btc.GetBlockCount()
+	blockHash, err1 := btc.GetBlockHash(blockHeight)
+	if err1 != nil {
+		return
+	}
+
+	block, err2 := btc.GetBlock(blockHash)
+	if err2 != nil {
+		return
+	}
+
+	blockOld, exist := MapBlock[blockHash.String()]
+	bytesOld, _ := json.Marshal(blockOld)
+	bytesNew, _ := json.Marshal(block)
+
+	if !exist || !bytes.Equal(bytesOld, bytesNew) {
+
+		MapBlock[blockHash.String()] = block
+
+		parse(block)
+	}
+
+}
+
+func parse(block *wire.MsgBlock) {
+
+	hashList, _ := block.TxHashes()
+
+	for _, txObj := range hashList {
+
+		tx := txObj.String()
+		result, err := btc.GetTransaction(tx)
+		if err != nil {
+			return
+		}
+
+		data := make(map[string]interface{})
+		data["token"] = "LTC"
+		data["transaction_id"] = result.TxID
+		data["transaction_fee"] = result.Fee
+
+		for _, obj := range result.Details {
+
+			if obj.Category == "send" {
+				data["from_address"] = obj.Address
+
+			} else if obj.Category == "receive" {
+
+				data["to_address"] = obj.Address
+				data["amount"] = obj.Amount
+
+				if de, ok := dbScan.HMAP_DEPOSIT[data["to_address"].(string)]; ok {
+					de.Notify(data)
+
+					dbScan.Report_Fees("LTC", result.Fee)
+					dbScan.Report_Deposit("LTC", obj.Amount)
+					dbScan.Report_Current("LTC")
+				}
+			}
+		}
+	}
+
+}
